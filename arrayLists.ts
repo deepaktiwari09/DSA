8bf2644f892996d4ce479215215616f1550a14a0
amitsoni <amit.soni@concertidc.com>
2023-06-19 10:16:35 +0530

    array list added for testing

---
+++ arrayLists.ts
@@ -0,0 +1,30 @@
+const simpleArray = [1, 2, 3, 4, 5];
+const stringArray = ['apple', 'banana', 'cherry', 'date', 'elderberry'];
+const objectArray = [
+    { id: 1, name: 'John' },
+    { id: 2, name: 'Alice' },
+    { id: 3, name: 'Bob' },
+    { id: 4, name: 'Jane' },
+    { id: 5, name: 'Mike' },
+];
+const multiArray = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+];
+const duplicateArray = [1, 2, 3, 4, 5, 3, 4, 2, 1, 5];
+const dateArray = [
+    new Date('2022-01-01'),
+    new Date('2022-02-01'),
+    new Date('2022-03-01'),
+    new Date('2022-04-01'),
+    new Date('2022-05-01'),
+  ];
+const nullUndefinedArray = [1, null, 3, undefined, 5];
+const mixedArray = [1, 'apple', true, { name: 'John' }, [1, 2, 3]];
+const sortedArray = [10, 20, 30, 40, 50];
+  
+
+
+let a = sortedArray.entries()
+a
